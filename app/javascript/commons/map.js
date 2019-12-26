4fcbdd970914df0cdf8e34ab300c80d99d3220d2
若山侑輝 <wkym_4a@icloud.com>
2019-12-26 16:56:27 +0900

    add_ユーザー情報更新機能（所在地へのピン立ても含めて）実装

--- app/javascript/commons/map.js
+++ app/javascript/commons/map.js
@@ -17,28 +17,127 @@ export function map_default_setting(map,lat,lon){
 
 }
 
-// export function map_btn_setting(flg,msg,lat,lon,btn_set,btn_reset,btn_reset,btn_save){
-//   msg.text=""//メッセージ枠は空欄に戻す
-//
-//   if(flg==false){
-//
-//     flg = true
-//     lat.readOnly = true;
-//     lon.readOnly = true;
-//     btn_set[0].style.backgroundColor = '#555555';
-//     btn_set[1].style.backgroundColor = '#555555';
-//     btn_reset.style.backgroundColor = '#A7F1FF';
-//     btn_save.disabled = false;
-//
-//   } else {
-//
-//     flg = false
-//     lat.readOnly = false;
-//     lon.readOnly = false;
-//     btn_set[0].style.backgroundColor = '#A7F1FF';
-//     btn_set[1].style.backgroundColor = '#A7F1FF';
-//     btn_reset.style.backgroundColor = '#555555';
-//     btn_save.disabled = true;
-//
-//   }
-// }
+
+// 地図から📍を消す
+export function map_delete_pins(map,pin_box){
+  if(pin_box.length > 0){
+    for (var i = 0; i < pin_box.length; i++) {
+      map.removeFeature(pin_box[i]);
+    }
+      pin_box = [];
+  }
+}
+
+
+// 地図に📍を一つ作る
+export function map_make_one_pin_no_content(map,lat,lon,pin_box){
+
+  var current_location = new Y.LatLng(lat.value,lon.value);
+
+  var marker = new Y.Marker(current_location);
+  // var marker = new Y.Marker(current_location,{title: .pins[i].pin_name});
+  map.addFeature(marker);
+
+  // // 作成したマーカーを保存
+  pin_box.push(marker);
+
+  // ピンの場所に移動
+  map.panTo(current_location, true);
+
+}
+
+export function map_btn_setting(flg,msg,lat,lon,btn_set,btn_reset,btn_save){
+  msg.text=""//メッセージ枠は空欄に戻す
+
+  if(flg[0]==false){
+  // 座標を設定した
+  // →変更前のフラグが「座標がまだ設定されていない」だった場合
+    flg[0] = true
+    lat.readOnly = true;
+    lon.readOnly = true;
+    btn_set[0].style.backgroundColor = '#555555';
+    btn_set[1].style.backgroundColor = '#555555';
+    btn_reset.style.backgroundColor = '#A7F1FF';
+    btn_save.disabled = false;
+
+  } else {
+  // 座標設定を解除した
+  // →変更前のフラグが「座標がもう設定されている」だった場合
+
+    flg[0] = false
+    lat.readOnly = false;
+    lon.readOnly = false;
+    btn_set[0].style.backgroundColor = '#A7F1FF';
+    btn_set[1].style.backgroundColor = '#A7F1FF';
+    btn_reset.style.backgroundColor = '#555555';
+    btn_save.disabled = true;
+
+  }
+}
+
+// 地
+export function map_set_address(msg,lat,lon,address){
+
+  var current_location = new Y.LatLng(lat.value,lon.value)
+  var request = { "latlng": current_location };
+
+  var geocoder = new Y.GeoCoder();
+
+  geocoder.execute( request , function(ydf) {
+      if ( ydf.features.length > 0 ) {
+          var feature = ydf.features[0];
+
+          if(feature.property.Address==""){
+            address.value=feature.property.Country.Name;
+          }else{
+            address.value=feature.property.Address;
+          }
+
+      }else{
+          msg.text="座標から住所を獲得できませんでした。住所は手入力で入力してください。";
+      }
+
+  } );
+}
+
+
+export function map_set_latlon(flg,map,msg,lat,lon,address,btn_sets,btn_reset,btn_save,pin_box){
+
+  var map_in_g = map
+  var request = { query : address.value };
+  var geocoder = new Y.GeoCoder();
+
+  geocoder.execute( request , function( ydf ) {
+    if ( ydf.features.length > 0 ) {
+
+      lat.value =ydf.features[0]["latlng"]["Lat"];
+      lon.value =ydf.features[0]["latlng"]["Lon"];
+
+      ////// ↓マーカー作成→既存マーカー削除→作成したメーカーを設置→作成マーカーを保存
+      var current_location = new Y.LatLng(lat.value,lon.value)
+
+      var marker = new Y.Marker(current_location);
+
+      //表示しているピンを消す
+      map_delete_pins(map,pin_box)
+
+      // 新たなピンを表示
+      map.addFeature(marker);
+
+      // // 作成したピンを保存
+      pin_box.push(marker);
+
+      // ピンの場所に移動
+      map.panTo(current_location, true);
+      ////// ↑マーカー作成→既存マーカー削除→作成したメーカーを設置→作成マーカーを保存
+
+      // ボタン設定を行う
+      map_btn_setting(flg,msg,lat,lon,btn_sets,btn_reset,btn_save);
+
+    }else{
+      //【住所から座標を獲得できなかった場合の処理】
+      msg.text="住所から座標を獲得できませんでした。"
+      return;
+    }
+  } );
+}
--- app/javascript/controllers/set_pin_controller.js
+++ app/javascript/controllers/set_pin_controller.js
@@ -1,26 +1,148 @@
 import { Controller } from "stimulus"
 
-import { map_default_setting , map_btn_setting } from "../commons/map"
+import { map_default_setting , map_btn_setting , map_make_one_pin_no_content ,map_delete_pins , map_set_address ,map_set_latlon } from "../commons/map"
 
-// マーカー保存用の枠
-var markers = [];
+// 📍保存用の枠
+var pin_box = [];
 
 // 座標が確定されているかどうが」のフラグ
-var latlng_decided_flg = false;
+var latlng_decided_flg = [false];
 
 export default class extends Controller {
-  static targets = ["map","flg","lat","lon","btn_set","btn_reset","address","btn_save","msg" ]
+  static targets = ["flg","map","lat","lon","btn_set","btn_reset","address","btn_save","msg" ]
 
-    initialize(e) {
+    connect(e) {
       this.map = new Y.Map(this.mapTarget.id,{configure : {
                   scrollWheelZoom : true
                 }});
 
-    // 地図の初期設定を行う
-    map_default_setting(this.map,this.latTarget,this.lonTarget);
+      // 地図の初期設定を行う
+      map_default_setting(this.map,this.latTarget,this.lonTarget);
 
-    // map_btn_setting();
+      //表示しているピンを消す(念の為、リロード時用？)
+      map_delete_pins(this.map,pin_box)
+
+      // ピンを建てる
+      map_make_one_pin_no_content(this.map,this.latTarget,this.lonTarget,pin_box)
+
+      // ボタン設定を行う
+      map_btn_setting(latlng_decided_flg,this.msgTarget,this.latTarget,this.lonTarget,this.btn_setTargets,this.btn_resetTarget,this.btn_saveTarget);
+
+      /////////////////////////////////////
+      var lat_dbc = this.latTarget
+      var lon_dbc = this.lonTarget
+      var address_dbc = this.addressTarget
+      var map_dbc = this.map
+
+          //地図がダブルクリックされた時の処理
+          this.map.bind('dblclick', function(e) {
+
+            // すでに座標が確定されている場合は、処理せず抜ける
+            if(latlng_decided_flg[0]==true){
+              return;
+              }
+
+            lat_dbc.value = e.Lat;
+            lon_dbc.value = e.Lon;
+            address_dbc.value = ""//「住所」を初期化……「座標→住所」で設定し直すため
+
+            //表示しているピンを消す(念の為、リロード時用？)
+            map_delete_pins(map_dbc,pin_box)
+
+            // ピンを建てる
+            map_make_one_pin_no_content(map_dbc,lat_dbc,lon_dbc,pin_box)
+
+            // var current_location = new Y.LatLng(e.Lat,e.Lon)
+            //
+            // var marker = new Y.Marker(current_location);
+            //
+            // map_box.addFeature(marker);
+            //
+            // if(markers.length > 0){
+            //   for (var i = 0; i < markers.length; i++) {
+            //     map_box.removeFeature(markers[i]);
+            //     // markers[i].setMap(null);
+            //   }
+            //     markers = [];	//参照を開放
+            // }
+            // // // 作成したマーカーを保存
+            // markers.push(marker);
+            //
+            // // ピンの場所に移動
+            // map_box.panTo(current_location, true);
+
+          });
 
     }
 
+    // 「座標確定解除」処理
+  reset_latlon(){
+    // まだ座標が確定されていない場合は、処理せず抜ける
+    if(latlng_decided_flg[0].value==false){
+      return;
+    }
+    // ボタン設定を行う
+    map_btn_setting(latlng_decided_flg,this.msgTarget,this.latTarget,this.lonTarget,this.btn_setTargets,this.btn_resetTarget,this.btn_saveTarget);
+
   }
+
+  // 【座標→住所】による「座標確定」処理
+  set_address_by_latlon(){
+    // すでに座標が確定されている場合は、処理せず抜ける
+    if(latlng_decided_flg[0].value==true){
+      return;
+      }
+
+    // ----↓↓緯度,経度に関するバリデーション処理↓↓----
+    if((this.latTarget.value == "") || (this.lonTarget.value == "") ){
+      this.msgTarget.text='緯度、経度を入力してください。'
+      return;
+    }
+
+    if(isNaN(this.latTarget.value)==true || isNaN(this.lonTarget.value)==true ){
+      this.msgTarget.text="緯度、経度には数値を入力してください。"
+      return;
+    }
+
+    if((this.latTarget.value < -90) || (this.latTarget.value > 90) ||
+     (this.lonTarget.value < -180) || (this.lonTarget.value > 180) ){
+      this.msgTarget.text='存在しない座標です。\n緯度は「-90」〜「90」、経度は「-180」〜「180」の範囲で入力してください。'
+      return;
+    }
+      // ----↑↑緯度,経度に関するバリデーション処理↑↑----
+
+    //表示しているピンを消す(念の為、リロード時用？)
+    map_delete_pins(this.map,pin_box)
+
+    // ピンを建てる+ピンの場所に移動
+    map_make_one_pin_no_content(this.map,this.latTarget,this.lonTarget,pin_box)
+
+    // 住所を表示する
+    map_set_address(this.msgTarget,this.latTarget,this.lonTarget,this.addressTarget)
+
+    // ボタン設定を行う
+    map_btn_setting(latlng_decided_flg,this.msgTarget,this.latTarget,this.lonTarget,this.btn_setTargets,this.btn_resetTarget,this.btn_saveTarget);
+  }
+
+    // 【住所→座標】による「座標確定」処理
+    set_latlon_by_address(){
+      // すでに座標が確定されている場合は、処理せず抜ける
+      if(latlng_decided_flg[0].value==true){
+        return;
+        }
+
+      // ----↓↓緯度,経度に関するバリデーション処理↓↓----
+      // 住所が存在しない場合は、エラーメッセージを表示して抜ける
+      if(this.addressTarget.value == ""){
+        this.msgTarget.text="住所を入力してください。";
+        return;
+      }
+      // ----↑↑緯度,経度に関するバリデーション処理↑↑----
+
+
+      // 住所からの座標設定を行う
+      map_set_latlon(latlng_decided_flg,this.map,this.msgTarget,this.latTarget,this.lonTarget,this.addressTarget,this.btn_setTargets,this.btn_resetTarget,this.btn_saveTarget,pin_box);
+
+    }
+
+}
