5541a0f7c891218bd7921a8ab3182e18db5d44aa
若山侑輝 <wkym_4a@icloud.com>
2020-03-24 18:44:42 +0900

    add_トップの説明画面を追加（途中

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,2 +1,11 @@
 class TopController < ApplicationController
+
+  def form
+    @user = User.find(1)
+    #「空き有り」「空き予定」「空き無し」の全てを表示（「空き無し」は灰色のピンとする……js側で処理）
+    @parkings = @user.parkings
+    # #「空き有り」「空き予定」のみ表示（「空き無し」は表示しない）
+    # @parkings = @user.parkings.where("status in (0,1)")
+  end
+
 end
