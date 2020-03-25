14dc66f90b74c202da16519fb2104ae8bcd8461d
若山侑輝 <wkym_4a@icloud.com>
2020-03-26 00:26:30 +0900

    add_トップの操作方法説明画面を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+  sessions: 'users/sessions',
+  registrations: "users/registrations",
+  }
+
   root 'top#form'
 
   get '/err', to: 'err#err'
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
-  # devise_for :users
-  devise_for :users, controllers: {
-  sessions: 'users/sessions',
-  registrations: "users/registrations",
-}
   # ユーザープロフィール画面は独自に作成する
   resources :users, only: [:show,:edit,:update] do
     member do
